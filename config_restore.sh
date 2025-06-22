8305a01e09ccf298e172f2a62acc15b82f46079a
chhartmann <chhartmann@web.de>
2025-06-22 12:57:09 +0000

    delete unused files from overlay before building

--- config_restore.sh
+++ config_restore.sh
@@ -2,3 +2,6 @@
 cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini-IPC.mk
 cp myconfig/buildroot_defconfig sysdrv/source/buildroot/buildroot-2023.02.6/configs/luckfox_pico_defconfig
 cp myconfig/kernel_defconfig sysdrv/source/kernel/arch/arm/configs/luckfox_rv1106_linux_defconfig
+
+rm project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-init/etc/init.d/S99usb0config
+rm -rf project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-shadow/etc/samba
