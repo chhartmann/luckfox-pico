30edaf4448077744c277cfab88d26ccc586a8a3c
chhartmann <chhartmann@web.de>
2025-06-29 06:41:27 +0000

    include wifi in interfaces config file as overlay

--- .github/workflows/config_restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp myconfig/.BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini-IPC.mk
-cp myconfig/buildroot_defconfig sysdrv/source/buildroot/buildroot-2023.02.6/configs/luckfox_pico_defconfig
-cp myconfig/dts_config sysdrv/source/kernel/arch/arm/boot/dts/rv1103g-luckfox-pico-mini.dts
-cp myconfig/kernel_defconfig sysdrv/source/kernel/arch/arm/configs/luckfox_rv1106_linux_defconfig
--- config_restore.sh
+++ config_restore.sh
@@ -3,5 +3,7 @@ cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buil
 cp myconfig/buildroot_defconfig sysdrv/source/buildroot/buildroot-2023.02.6/configs/luckfox_pico_defconfig
 cp myconfig/kernel_defconfig sysdrv/source/kernel/arch/arm/configs/luckfox_rv1106_linux_defconfig
 
+cp -r myconfig/overlay/* project/cfg/BoardConfig_IPC/overlay/overlay-myconfig
+
 rm project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-init/etc/init.d/S99usb0config
 rm -rf project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-shadow/etc/samba
