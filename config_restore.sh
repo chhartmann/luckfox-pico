f94ad84cbe82393a6e85fe78c019b44ec5dc57a1
chhartmann <chhartmann@web.de>
2025-06-29 09:34:18 +0000

    patch config before build is started

--- config_restore.sh
+++ config_restore.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini-IPC.mk
+
+# restore defconfigs
 cp myconfig/buildroot_defconfig sysdrv/source/buildroot/buildroot-2023.02.6/configs/luckfox_pico_defconfig
 cp myconfig/kernel_defconfig sysdrv/source/kernel/arch/arm/configs/luckfox_rv1106_linux_defconfig
 
-cp -r myconfig/overlay/* project/cfg/BoardConfig_IPC/overlay/overlay-myconfig
+# create config pico mini
+printf '1\n1\n0\n' | ./build.sh conf
 
+# patch config and filesystem
+cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini-IPC.mk
+cp -r myconfig/overlay/* project/cfg/BoardConfig_IPC/overlay/overlay-myconfig
 rm project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-init/etc/init.d/S99usb0config
 rm -rf project/cfg/BoardConfig_IPC/overlay/overlay-luckfox-buildroot-shadow/etc/samba
+
+# now build can be started
+# ./build.sh
