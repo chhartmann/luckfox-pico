6c476598c1e6e83cd90968505413369ba5493999
chhartmann <chhartmann@web.de>
2025-08-04 12:30:00 +0200

    Update config_restore.sh for maxpro

--- config_restore.sh
+++ config_restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # restore defconfigs
-cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini-IPC.mk
+cp myconfig/BoardConfig.mk project/cfg/BoardConfig_IPC/project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-Buildroot-RV1106_Luckfox_Pico_Pro_Max-IPC.mk
 cp -r myconfig/overlay/* project/cfg/BoardConfig_IPC/overlay/overlay-myconfig
 cp myconfig/kernel_defconfig sysdrv/source/kernel/arch/arm/configs/luckfox_rv1106_linux_defconfig
 
