a6cc826357762158e17d67b221722d3444526667
chhartmann <chhartmann@web.de>
2025-08-11 18:29:56 +0000

    remove oem apps and rockchip tests, add flask

--- myconfig/BoardConfig.mk
+++ myconfig/BoardConfig.mk
@@ -98,10 +98,10 @@ export RK_CAMERA_SENSOR_CAC_BIN="CAC_sc4336_OT01_40IRC_F16"
 #export RK_APP_IPCWEB_BACKEND=y
 
 # enable install app to oem partition
-export RK_BUILD_APP_TO_OEM_PARTITION=y
+export RK_BUILD_APP_TO_OEM_PARTITION=n
 
 # enable rockchip test
-export RK_ENABLE_ROCKCHIP_TEST=y
+export RK_ENABLE_ROCKCHIP_TEST=n
 
 # enable rockchip wifi
 export RK_ENABLE_WIFI=y
